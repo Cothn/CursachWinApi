4d5f8ab20d30ae89430b989df3f672b925dfc092
Cothn <Sevkaa-0013@mail.ru>
2019-11-23 19:29:30 +0400

    Add(+-MAxSpeed, +-CountEnemy)
    SomeCodeRedact

--- Xonix/WinApiLab1/Source.cpp
+++ Xonix/WinApiLab1/Source.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include <wingdi.h>
 
+
 typedef struct GameObject
 {
 	int delta_x;
@@ -10,61 +11,65 @@ typedef struct GameObject
 
 } GameObject;
 
+class stack
+{
+	int obj[Width * Height * 2 + 1];
+public:
+	int top;
+	stack()
+	{
+		top = 0;
+	}
+	void push(int i)
+	{
+		obj[top] = i;
+		top++;
+	}
+	int pop()
+	{
+		return(obj[--top]);
+	}
+};
 
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 void MyRedrawWindow(HWND hWnd);
 void InitializeGame(GameObject* player, GameObject* enemy, int x, int y, int edge);
 void InitializeEnemy(GameObject* objects, int edge);
-//void SetPlayerSpeed(ControlObject* object, int speed_x, int speed_y);
 void MovePlayer(GameObject* object, GameObject* enemy, int width, int height);
 void MoveEnemy(GameObject* objects);
-void DrawGameField(GameObject* object, GameObject* enemy, HDC hdc, HDC hdcMemSurface);
+void DrawGameField(GameObject* object, GameObject* enemy, HDC hdc);
 void UpdateField();
-void drop(int y, int x);
+void CheckCell(int y, int x);
 
-static LPSTR OverMessage = (LPSTR)"Game Over!";
-static LPSTR WinMessage = (LPSTR)"Player Win!";
-static int CELL_SIZE = 15;
-static int ENEMY_MAX_SPEED = (12 % (CELL_SIZE)) * 2;
-static GameObject player;
+
+//константы приложения
+static const LPSTR OverMessage = (LPSTR)"Game Over!";
+static const LPSTR WinMessage = (LPSTR)"Player Win!";
+static const int CELL_SIZE = 15;
+const int Height = 35;
+const int Width = 40;
+const int WIN_PERCENT = 70;
+const int MaxCountEnemy = Width / 2;
+
+//флаги
+bool GameEnd = false;
+bool PlayerWin = false;
+
+//изменяемые параметры
+int CountEnemy =5;
+int EnemyMaxSpeed = (12 % (CELL_SIZE));
+
+//размеры окна в пикселях
+const int pixelWidth = 1 + (Width + 1) * CELL_SIZE;
+const int pixelHeight = 24 + (Height + 1) * CELL_SIZE;
+
+//Глобальные переменные
+int gameField[Height][Width];
+
+
 
 static HDC hdcMemSurface;
-static bool GameEnd = false;
-static bool PlayerWin = false;
-const int M = 35;
-const int N = 40;
-const int WIN_PERCENT = 70;
-int CountEnemy =5;
-
-int gameField[M][N];
-GameObject* Enemys;
-class stack
-{
-	int a[N * M * 2+1];
-	public:
-		int top;
-		stack()
-		{
-			top = 0;
-		}
-		void push(int i)
-		{
-			a[top] = i;
-			top++;
-		}
-		int pop() 
-		{
-			return(a[--top]);
-		}
-};
-
-stack s;
-
-//stack::stack()
-//{
-//	top = 0;
-//}
 
 
 
@@ -73,26 +78,22 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdL
 	WNDCLASSEX wcex; HWND hWnd; MSG msg;
 
 	wcex.cbSize = sizeof(WNDCLASSEX);
-	wcex.style = CS_DBLCLKS;
+	wcex.style = NULL;
 	wcex.lpfnWndProc = WndProc;
 	wcex.cbClsExtra = 0;
 	wcex.cbWndExtra = 0;
 	wcex.hInstance = hInstance;
-	wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wcex.hIcon = NULL;
 	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wcex.hbrBackground = (HBRUSH)(COLOR_SCROLLBAR + 1);
 	wcex.lpszMenuName = NULL;
-	wcex.lpszClassName = "HelloKsisClass";
+	wcex.lpszClassName = "XonixClass";
 	wcex.hIconSm = wcex.hIcon;
 	RegisterClassEx(&wcex);
 
-	hWnd = CreateWindow("HelloKsisClass", "Hello, Xonix!",
-		WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
-		1+(N+1)*CELL_SIZE, 24+(M+1)*CELL_SIZE, NULL, NULL, hInstance, NULL);
-
-
-	Enemys = new GameObject[N / 2];
-	InitializeGame(&player, Enemys, 0, 0, 1);
+	hWnd = CreateWindow("XonixClass", "Hello, Xonix!",
+		WS_OVERLAPPED | WS_SYSMENU, CW_USEDEFAULT, CW_USEDEFAULT,
+		pixelWidth, pixelHeight, NULL, NULL, hInstance, NULL);
 
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
@@ -109,6 +110,9 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdL
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
 	WPARAM wParam, LPARAM lParam)
 {
+	GameObject* Enemys;
+	GameObject player;
+
 	HDC hmdc;
 	HBRUSH hBrush; //создаём объект-кисть
 	PAINTSTRUCT ps;
@@ -119,86 +123,130 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
 	switch (message)
 	{
 
-	case WM_TIMER:
-		if (!GameEnd) {
-			MovePlayer(&player, Enemys, N, M);
-		}
-		else
+		case WM_CREATE:
 		{
-			GameEnd = false;
+			// инициализация игры
+			Enemys = new GameObject[MaxCountEnemy];
+			InitializeGame(&player, Enemys, 0, 0, 1);
 
-			if(PlayerWin)
+			const HINSTANCE hInstance = (HINSTANCE)GetWindowLong(hWnd, GWL_HINSTANCE);
+			const HDC hdcWin = GetDC(hWnd);
+			hdcMemSurface = CreateCompatibleDC(hdcWin);
+			ReleaseDC(hWnd, hdcWin);
+			SetTimer(hWnd, 1, 1000/15, NULL);
+			return 0;
+		}
+
+		case WM_TIMER:
+		{
+			if (!GameEnd) {
+				MovePlayer(&player, Enemys, Width, Height);
+			}
+			else
 			{
-				PlayerWin = false;
-				MessageBox(hWnd, WinMessage, WinMessage, MB_OK | MB_APPLMODAL);
-				InitializeGame(&player, Enemys, 0, 0, 1);
-			}
-			else {
-				InitializeGame(&player, Enemys, 0, 0, 1);
-				MessageBox(hWnd, OverMessage, OverMessage, MB_OK | MB_APPLMODAL);
-			}
+				GameEnd = false;
 
+				if (PlayerWin)
+				{
+					PlayerWin = false;
+					MessageBox(hWnd, WinMessage, WinMessage, MB_OK | MB_APPLMODAL);
+					InitializeGame(&player, Enemys, 0, 0, 1);
+				}
+				else {
+					InitializeGame(&player, Enemys, 0, 0, 1);
+					MessageBox(hWnd, OverMessage, OverMessage, MB_OK | MB_APPLMODAL);
+				}
+
+			}
+			MyRedrawWindow(hWnd);
+			break;
 		}
-		MyRedrawWindow(hWnd);
-		break;
 
-	case WM_CREATE:
-	{
-		const HINSTANCE hInstance = (HINSTANCE)GetWindowLong(hWnd, GWL_HINSTANCE);
-		const HDC hdcWin = GetDC(hWnd);
-		hdcMemSurface = CreateCompatibleDC(hdcWin);
-		ReleaseDC(hWnd, hdcWin);
-		SetTimer(hWnd, 1, 1000/15, NULL);
-		return 0;
-	}
-
-	case WM_ERASEBKGND:
-		return 1;
-	case WM_KEYDOWN:
-	{
-		switch (wParam)
+		case WM_KEYDOWN:
 		{
-		case VK_LEFT:
-			player.delta_x = -1;
-			player.delta_y = 0;
-			break;
-		case VK_UP:
-			player.delta_x = 0;
-			player.delta_y = -1;
-			break;
-		case VK_RIGHT:
-			player.delta_x = 1;
-			player.delta_y = 0;
-			break;
-		case VK_DOWN:
-			player.delta_x = 0;
-			player.delta_y = 1;
-			break;
-		default:
+			switch (wParam)
+			{
+			case VK_LEFT:
+				player.delta_x = -1;
+				player.delta_y = 0;
+				break;
+			case VK_UP:
+				player.delta_x = 0;
+				player.delta_y = -1;
+				break;
+			case VK_RIGHT:
+				player.delta_x = 1;
+				player.delta_y = 0;
+				break;
+			case VK_DOWN:
+				player.delta_x = 0;
+				player.delta_y = 1;
+				break;
+			default:
+				break;
+			}
+
+
+			MyRedrawWindow(hWnd);
 			break;
 		}
 
+		case WM_KEYUP:
+		{
+			switch (wParam)
+			{
+			case VK_ADD:
+				if (CountEnemy < MaxCountEnemy)
+				{
+					CountEnemy++;
+					InitializeGame(&player, Enemys, 0, 0, 1);
+				}
+				break;
+			case VK_SUBTRACT:
+				if (CountEnemy > 0)
+				{
+					CountEnemy--;
+					InitializeGame(&player, Enemys, 0, 0, 1);
+				}
+				break;
+			case VK_MULTIPLY:
+				if (EnemyMaxSpeed < CELL_SIZE)
+				{
+					EnemyMaxSpeed++;
+				}
+				break;
+			case VK_DIVIDE:
+				if (EnemyMaxSpeed > 1)
+				{
+					EnemyMaxSpeed--;
+				}
+				break;
+			case VK_END:
+				InitializeGame(&player, Enemys, 0, 0, 1);
+				break;
 
-		MyRedrawWindow(hWnd);
-		break;
-	}
+			default:
+				break;
+			}
+		}
 
-	case WM_PAINT:
-	{
-		hdc = BeginPaint(hWnd, &ps);
+		case WM_ERASEBKGND:
+			return 1;
 
-		DrawGameField(&player, Enemys, hdc, hdcMemSurface);
-		EndPaint(hWnd, &ps);
-		break;
-	}
+		case WM_PAINT:
+		{
+			hdc = BeginPaint(hWnd, &ps);
+			DrawGameField(&player, Enemys, hdc);
+			EndPaint(hWnd, &ps);
+			break;
+		}
 
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
 
-	default:
-
-		return DefWindowProc(hWnd, message, wParam, lParam);
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
 	}
 	return 0;
 }
@@ -209,14 +257,12 @@ void MyRedrawWindow(HWND hWnd)
 	UpdateWindow(hWnd);
 }
 
-
 void InitializeGame(GameObject* player, GameObject* enemy, int x, int y, int edge)
 {
-
 	//инициализация края
-	for (int i = 0; i < M; i++)
-		for (int j = 0; j < N; j++)
-			if (i < edge || j < edge || i >= M - edge || j >= N - edge)  gameField[i][j] = 1;
+	for (int i = 0; i < Height; i++)
+		for (int j = 0; j < Width; j++)
+			if (i < edge || j < edge || i >= Height - edge || j >= Width - edge)  gameField[i][j] = 1;
 			else gameField[i][j] = 0;
 			
 
@@ -234,18 +280,17 @@ void InitializeEnemy(GameObject* objects, int edge)
 	//Инициализация противников
 	for (int i = 0; i < CountEnemy; i++)
 	{
-		objects[i].x = (edge + rand() % (N - edge * 2))* CELL_SIZE;
-		objects[i].y = (edge + rand() % (M - edge * 2))* CELL_SIZE;
-		objects[i].delta_x = ENEMY_MAX_SPEED / 2 - rand() % ENEMY_MAX_SPEED;
-		objects[i].delta_y = ENEMY_MAX_SPEED / 2 - rand() % ENEMY_MAX_SPEED;
+		objects[i].x = (edge + rand() % (Width - edge * 2))* CELL_SIZE;
+		objects[i].y = (edge + rand() % (Height - edge * 2))* CELL_SIZE;
+		objects[i].delta_x = EnemyMaxSpeed - rand() % (EnemyMaxSpeed * 2 + 1);
+		objects[i].delta_y = EnemyMaxSpeed - rand() % (EnemyMaxSpeed * 2 + 1);
 	}
 }
 
 void MovePlayer(GameObject* object, GameObject* enemy, int width, int height)
 {
-
-	//проверка конечной точки
 	if ((object->delta_x != 0) || (object->delta_y != 0)) {
+
 
 		object->x += object->delta_x;
 		if ((object->x) < 0)
@@ -267,6 +312,7 @@ void MovePlayer(GameObject* object, GameObject* enemy, int width, int height)
 			object->y = height - 1;
 		}
 
+		//проверка конечной точки
 		switch (gameField[object->y][object->x])
 		{
 			case 0:
@@ -276,15 +322,16 @@ void MovePlayer(GameObject* object, GameObject* enemy, int width, int height)
 			}
 			case 1:
 			{
+				//проверяем наличие "хвоста"
 				if (gameField[object->y- object->delta_y][object->x - object->delta_x] == 2) {
 					object->delta_x = 0;
 					object->delta_y = 0;
 
-					//определение свободных областей
+					//определение вражеской територии
 					for (int i = 0; i < CountEnemy; i++)
-						drop(Enemys[i].x / CELL_SIZE, Enemys[i].y / CELL_SIZE);
+						CheckCell(enemy[i].x / CELL_SIZE, enemy[i].y / CELL_SIZE);
 
-					UpdateField(); //обновление игрового поля
+					UpdateField();
 				}
 				break;
 
@@ -300,55 +347,52 @@ void MovePlayer(GameObject* object, GameObject* enemy, int width, int height)
 	MoveEnemy(enemy);
 }
 
-
-
-void drop(int x, int y)
+void CheckCell(int x, int y)
 {
+	stack CordinateStack;
 
-	s.push(x);
-	s.push(y);
-	while (s.top != 0) {
+	CordinateStack.push(x);
+	CordinateStack.push(y);
+	while (CordinateStack.top != 0) {
 		if (gameField[y][x] == 0) gameField[y][x] = -1;
 		if (gameField[y - 1][x] == 0)
 		{
-			s.push(x);
-			s.push(y);
+			CordinateStack.push(x);
+			CordinateStack.push(y);
 			y--;
 		}
 		else if (gameField[y + 1][x] == 0)
 		{
-			s.push(x);
-			s.push(y);
+			CordinateStack.push(x);
+			CordinateStack.push(y);
 			y++;
 		}
 		else if (gameField[y][x - 1] == 0)
 		{
-			s.push(x);
-			s.push(y);
+			CordinateStack.push(x);
+			CordinateStack.push(y);
 			x--;
 		}
 		else if (gameField[y][x + 1] == 0)
 		{
-			s.push(x);
-			s.push(y);
+			CordinateStack.push(x);
+			CordinateStack.push(y);
 			x++;
 		}
 		else
 		{
-			y = s.pop();
-			x = s.pop();
+			y = CordinateStack.pop();
+			x = CordinateStack.pop();
 		}
 	}
 
 }
 
-
-
 void UpdateField()
 {
 	int ColEnemyCell= 0;
-	for (int i = 0; i < M; i++)
-		for (int j = 0; j < N; j++)
+	for (int i = 0; i < Height; i++)
+		for (int j = 0; j < Width; j++)
 			if (gameField[i][j] == -1)
 			{
 				gameField[i][j] = 0;
@@ -357,7 +401,7 @@ void UpdateField()
 			else gameField[i][j] = 1;
 
 	//проверка на победу игрока
-	float EnemyPercent = ((float)ColEnemyCell / (N * M))*100;
+	float EnemyPercent = ((float)ColEnemyCell / (Width * Height))*100;
 	if (EnemyPercent < 100 - WIN_PERCENT)
 	{
 		GameEnd = true;
@@ -390,13 +434,12 @@ void MoveEnemy(GameObject* objects)
 			|| (gameField[objects[i].y / CELL_SIZE + 1][objects[i].x / CELL_SIZE ] == 1))
 		{
 			objects[i].delta_y = -objects[i].delta_y;
-			objects[i].y += objects[i].delta_y;
 			if (objects[i].delta_y > 0) {
 				objects[i].y += (CELL_SIZE - objects[i].y % CELL_SIZE) * 2; //отскок вниз
 			}
 			else
 			{
-				objects[i].y -= (objects[i].y % CELL_SIZE) * 2; //отскок вверх
+				objects[i].y += (objects[i].y % CELL_SIZE) * -2; //отскок вверх
 			}
 		}
 
@@ -409,23 +452,20 @@ void MoveEnemy(GameObject* objects)
 
 }
 
-
-
-void DrawGameField(GameObject* object, GameObject* enemy, HDC hdc,  HDC hdcMemSurface)
+void DrawGameField(GameObject* object, GameObject* enemy, HDC hdc)
 {
-
+	//буферизация
 	HDC BuffHdc = CreateCompatibleDC(hdc);
-	HBITMAP BuffHbm = CreateCompatibleBitmap(hdc, 1 + (N + 1) * CELL_SIZE, 24 + (M + 1) * CELL_SIZE);
+	HBITMAP BuffHbm = CreateCompatibleBitmap(hdc, pixelWidth, pixelHeight);
 	HANDLE BuffHan = SelectObject(BuffHdc, BuffHbm);
 
-	//перерисовка поля
-	for (int i = 0; i < M; i++)
+	//перерисовка игрового поля
+	for (int i = 0; i < Height; i++)
 	{
-		for (int j = 0; j < N; j++) {
+		for (int j = 0; j < Width; j++) {
 			if (gameField[i][j] == 1)
 			{
 				Rectangle(BuffHdc, j * CELL_SIZE, i * CELL_SIZE, j * CELL_SIZE + CELL_SIZE, i * CELL_SIZE + CELL_SIZE);
-
 			}
 			if (gameField[i][j] == 2)
 			{
@@ -447,7 +487,7 @@ void DrawGameField(GameObject* object, GameObject* enemy, HDC hdc,  HDC hdcMemSu
 
 	}
 
-	BitBlt(hdc, 0, 0, 1 + (N + 1) * CELL_SIZE, 24 + (M + 1) * CELL_SIZE, BuffHdc, 0, 0, SRCCOPY);
+	BitBlt(hdc, 0, 0, 1 + (Width + 1) * CELL_SIZE, 24 + (Height + 1) * CELL_SIZE, BuffHdc, 0, 0, SRCCOPY);
 
 	//освобождаем память
 	SelectObject(BuffHdc, BuffHan);
